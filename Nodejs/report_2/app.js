5558cd72ed946c36ec27e9fa154c5f53bd0e4458
park singyu <psk876@naver.com>
2019-12-21 16:02:21 +0900

    회원가입

--- Nodejs/report_2/app.js
+++ Nodejs/report_2/app.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var app = express()
 var router = require('./router/router')
+var bodyParser = require('body-parser')
 
 app.listen(8080,function(){
     console.log('server start!!!')
 })
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 
 app.use(router)
--- Nodejs/report_2/router/member/memberRouter.js
+++ Nodejs/report_2/router/member/memberRouter.js
@@ -1,12 +1,48 @@
 var express = require('express')
 var router = express.Router()
+var mysql = require('mysql')
+const crypto = require('crypto')
+
+var connection = mysql.createConnection({
+    host    :   'localhost',
+    port    :   3306,
+    user    :   'root',
+    password:   'root',
+    database:   'jsman'
+});
+
+connection.connect();
+
+router.get('/joinForm',function(req,res){
+    res.render('member/joinForm.ejs')
+})
 
 router.get('/loginForm',function(req,res){
     res.render('member/loginForm.ejs')
 })
 
-router.get('/joinForm',function(req,res){
-    res.render('member/joinForm.ejs')
+
+router.post('/join',function(req,res){
+    var body = req.body;
+    var mb_id = body.mb_id;
+    var mb_pw = body.mb_pw;
+    var mb_email = body.mb_email;
+
+    mb_pw = crypto.createHash('sha512').update(mb_pw).digest('hex'); 
+
+    var query_params = {'mb_id' : mb_id,'mb_pw' : mb_pw,'mb_email' : mb_email}
+
+    var query = connection.query('INSERT INTO member SET ?', query_params, function (error, results, fields) {
+        var result;
+        if (error) result = { 'result' : 1 , 'message' : '회원가입 실패' , 'error' : error}
+        else result = { 'result' : 0 , 'message' : '회원가입 성공'}
+        res.json(result)
+    });
 })
+
+router.post('/login',function(req,res){
+    
+})
+
 
 module.exports = router;
